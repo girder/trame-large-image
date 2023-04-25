2fbbd49c474a8d7a3d26724b62e7017ba174df75
Bane Sullivan <bane.sullivan@kitware.com>
2023-04-24 20:44:41 -0400

    Fix imports

--- large_image_trame/widgets/__init__.py
+++ large_image_trame/widgets/__init__.py
@@ -1,1 +1,11 @@
-from .large_image_trame import *
+from .large_image_trame import (
+    GeoJSViewer,
+    LargeImageLeafletMap,
+    LargeImageLeafletTileLayer,
+)
+
+__all__ = [
+    "GeoJSViewer",
+    "LargeImageLeafletMap",
+    "LargeImageLeafletTileLayer",
+]
